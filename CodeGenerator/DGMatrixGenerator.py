da11137f2f17ff520da3972399a09f3c92ef58b5
Jean-Matthieu Gallard <ga96nuv3@mpp2-login5.cos.lrz.de>
2017-11-14 11:49:46 +0100

    BUGFIX CodeGen - wrong indices in DGMatrix

--- CodeGenerator/DGMatrixGenerator.py
+++ CodeGenerator/DGMatrixGenerator.py
@@ -85,9 +85,9 @@ class DGMatrixGenerator:
                 fineGridProjector1d_0[i][j] *= self.m_wGPN[i]/self.m_wGPN[j]/3.0
                 fineGridProjector1d_1[i][j] *= self.m_wGPN[i]/self.m_wGPN[j]/3.0
                 fineGridProjector1d_2[i][j] *= self.m_wGPN[i]/self.m_wGPN[j]/3.0
-        self.m_context['fineGridProjector1d_T_weighted_1'] = Utils.matrixPadAndFlatten_RowMajor(fineGridProjector1d_0,l_padSize)
-        self.m_context['fineGridProjector1d_T_weighted_2'] = Utils.matrixPadAndFlatten_RowMajor(fineGridProjector1d_1,l_padSize)
-        self.m_context['fineGridProjector1d_T_weighted_3'] = Utils.matrixPadAndFlatten_RowMajor(fineGridProjector1d_2,l_padSize)
+        self.m_context['fineGridProjector1d_T_weighted_0'] = Utils.matrixPadAndFlatten_RowMajor(fineGridProjector1d_0,l_padSize)
+        self.m_context['fineGridProjector1d_T_weighted_1'] = Utils.matrixPadAndFlatten_RowMajor(fineGridProjector1d_1,l_padSize)
+        self.m_context['fineGridProjector1d_T_weighted_2'] = Utils.matrixPadAndFlatten_RowMajor(fineGridProjector1d_2,l_padSize)
         
         #generate files 
         TemplatingUtils.renderAsFile('DGMatrices_h.template',   self.m_filenameRoot+'.h',   self.m_context)
