53604394e238325b53e2dc1d3f348506712ab85e
domcha <dominic.charrier@googlemail.com>
2017-12-07 17:02:07 +0000

    Benchmarks/*/CCZ4: Modify the projecpaths.cfg script to take distance to ExaHyPE-Engine into account.

---
+++ Benchmarks/hamilton/CCZ4/create-symlinks.sh
@@ -0,0 +1,9 @@
+ln -s ../../../AstroApplications/CCZ4/CCZ4Solver_ADERDG.cpp
+ln -s ../../../AstroApplications/CCZ4/CCZ4Solver_FV.cpp
+ln -s ../../../AstroApplications/CCZ4/Fortran
+ln -s ../../../AstroApplications/CCZ4/Helpers
+ln -s ../../../AstroApplications/CCZ4/InitialData
+ln -s ../../../AstroApplications/CCZ4/Writers
+ln -s ../../../AstroApplications/CCZ4/typesDef.f90
+
+source projectpaths.cfg
