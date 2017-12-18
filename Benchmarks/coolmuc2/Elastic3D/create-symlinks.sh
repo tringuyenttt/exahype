1f99c20d0933ba8916ca097d39e4e2feb7ef6e19
Dominic Charrier <dominic.e.charrier@durham.ac.uk>
2017-12-18 14:44:31 +0100

    Clean symlink file.

--- Benchmarks/coolmuc2/Elastic3D/create-symlinks.sh
+++ Benchmarks/coolmuc2/Elastic3D/create-symlinks.sh
@@ -3,7 +3,5 @@ ln -s ../../../ApplicationExamples/Elastic/Elastic3D/ConservedQuantitiesWriter.c
 ln -s ../../../ApplicationExamples/Elastic/Elastic3D/CurvilinearTransformation.cpp
 ln -s ../../../ApplicationExamples/Elastic/Elastic3D/ElasticWaveEquation.cpp
 ln -s ../../../ApplicationExamples/Elastic/Elastic3D/setUp.sh
-ln -s ../../../ApplicationExamples/Elastic/Elastic3D/ConservedQuantitiesWriter.h
-ln -s ../../../ApplicationExamples/Elastic/Elastic3D/CurvilinearTransformation.h
 ln -s ../../../ApplicationExamples/Elastic/Elastic3D/ElasticWaveEquation.h
 ln -s ../../../ApplicationExamples/Elastic/Elastic3D/topography.yaml
