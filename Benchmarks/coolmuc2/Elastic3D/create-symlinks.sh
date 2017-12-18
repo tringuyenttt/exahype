f3701dffdbad7e7f5d311533f852833e7bf5189f
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2017-12-18 11:28:26 +0000

    Benchmarks: Start adding Elastic3D benchmarks.

---
+++ Benchmarks/coolmuc2/Elastic3D/create-symlinks.sh
@@ -0,0 +1,10 @@
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/CurvilinearMesh_Filter.cpd
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/ConservedQuantitiesWriter.cpp
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/CurvilinearTransformation.cpp
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/ElasticWaveEquation.cpp
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/setUp.sh
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/build-A-executables.sh 
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/ConservedQuantitiesWriter.h
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/CurvilinearTransformation.h
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/ElasticWaveEquation.h
+ln -s ../../../ApplicationExamples/Elastic/Elastic3D/topography.yaml
