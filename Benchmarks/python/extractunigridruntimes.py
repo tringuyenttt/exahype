822b235ea51bdcb1e8fdd0650292e297f97e344d
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2017-12-19 15:00:09 +0000

    Clean up extract unigrid runtimes script.

--- Benchmarks/python/extractunigridruntimes.py
+++ Benchmarks/python/extractunigridruntimes.py
@@ -99,89 +99,85 @@ def extractRuntimes(table):
          If true, use old adapter names: ADERDGTimeStep,... Not the new ones: FusedTimeStep,...
     '''
     nonfusedAdapters = [ 
-                          "BroadcastGlobalDataAndMergeNeighbourMessages",
-                          "SolutionUpdate",
-                          "Prediction" # substract one; exclude initialisation
+                          'BroadcastGlobalDataAndMergeNeighbourMessages',
+                          'SolutionUpdate',
+                          'Prediction' # substract one; exclude initialisation
                        ]
     fusedAdapters    = [ 
-                        "FusedTimeStep",
-                        "PredictionRerun"
+                        'FusedTimeStep',
+                        'PredictionRerun'
                        ]
+                       
     
-    datafile    = open(table, 'r')
-    header      = next(datafile).strip()
-    reader      = csv.reader(datafile,delimiter='&')
+    data = readTable(table)
     
+    #                     0       1    2   3         4       5       6     7     8     9    10            11              12 
     # assume: row = [identifier,order,cc,kernels,algorithm,adapter,nodes,tasks,cores,mode,iterations,total user time, total cpu time]
-    sorted_data = sorted(reader, key=lambda x: (x[0],x[1],x[2],x[3],x[4],int(x[6]),int(x[7]),int(x[8]),x[5]))
-               
-    def only_adapter_changed(row,last_row):
-        return (row[0] == last_row[0] and 
-                row[1] == last_row[1] and 
-                row[2] == last_row[2] and 
-                row[3] == last_row[3] and 
-                row[4] == last_row[4] and
-                # row[5] == last_row[5] and # adapter
-                row[6] == last_row[6] and
-                row[7] == last_row[7] and
-                row[8] == last_row[8])
+    data = sorted(data, key=lambda x: (x[0],x[1],x[2],x[3],x[4],int(x[6]),int(x[7]),int(x[8]),x[5]))
     
-    # init ( we do the first row twice; a little inconsistent )
-    last_row = sorted_data[0]
-    result = last_row[:5] + last_row[6:]
-    result[9]  = '0 '  # iterations
-    result[10] = '0 '  # normalised user time
-    result[11] = '0'   # normalised fused time step time
-    result.append(0)   # predictor reruns
-    result.append('0.')# time per predictor rerun
+    identifiers = sorted(list(set(column(data,0))),key=str)
+    orders      = sorted(list(set(column(data,1))),key=int)
+    compilers   = sorted(list(set(column(data,2))),key=str)
+    kernels     = sorted(list(set(column(data,3))),key=str)
+    algorithms  = sorted(list(set(column(data,4))),key=str)
+    nodes       = sorted(list(set(column(data,6))),key=int)
+    tasks       = sorted(list(set(column(data,7))),key=int)
+    cores       = sorted(list(set(column(data,8))),key=int)
+    
+    # processing data
+    result = []
+    for identifier in identifiers:
+        for order in orders:
+            for compiler in compilers:
+                for kernel in kernels:
+                    for node in nodes:
+                        for task in tasks:
+                            for core in cores:
+                                for algorithm in algorithms:
+                                    averageRuntimePerIteration       = 0.0
+                                    averageRuntimePerFusedTimeStep   = 0.0
+                                    averageRuntimePerPredictionRerun = 0.0
+                                    
+                                    timesteps        = sys.maxsize
+                                    predictionReruns = 0
+                                    if algorithm=='fused':
+                                        for adapter in fusedAdapters:
+                                            filtered = list(filter(lambda x: \
+                                                x[0]==identifier and x[1]==order and x[2]==compiler and x[3]==kernel and \
+                                                x[4]==node and x[5]==adapter and x[6]==nodes and x[7]==tasks and \
+                                                x[8]==tasks, data))
+                                            
+                                            iterations                  = int ( filtered[0][10] )
+                                            averageRuntimePerIteration += float ( filtered[0][11] ) / iterations
+                                            
+                                            if adapter=='FusedTimeStep':  
+                                                timesteps                        = iterations
+                                                averageRuntimePerFusedTimeStep   = float ( filtered[0][11] ) / iterations
+                                            elif adapter=='PredictionRerun':
+                                                predictionRerurns                = iterations
+                                                averageRuntimePerPredictionRerun = float ( filtered[0][11] ) / iterations
+                                    
+                                    elif algorithm=='nonfused':
+                                        averageRuntimePerIteration = 0.0
+                                        timesteps = sys.maxsize
+                                        for adapter in nonfusedAdapters:
+                                            filtered = list(filter(lambda x: \
+                                                x[0]==identifier and x[1]==order and x[2]==compiler and x[3]==kernel and \
+                                                x[4]==node and x[5]==adapter and x[6]==nodes and x[7]==tasks and \
+                                                x[8]==tasks, data))
+                                            
+                                            timesteps                   = int    ( filtered[0][10] )
+                                            averageRuntimePerIteration += float ( filtered[0][11] ) / iterations
+                                            timesteps                   = min(minIterations,iterations)
+                                            
+                                    row = [identifier,order,cc,kernels,algorithm,nodes,tasks,cores,mode,timesteps,averageRuntimePerIteration,averageRuntimePerFusedTimeStep,predictionReruns,averageRuntimePerPredictionRerun]
+                                    result.append(row)
     
     # loop
     header = ["Mesh","Order","CC","Kernels","Algorithm","Nodes","Tasks (per Node)","Cores (per Task)","Mode","Iterations","Total Runtime (Per Iteration)","FusedTimeStep (Per Iteration)","Reruns", "Predictor Rerun (Per Iteration)"]
     filename = table.replace('.csv','.runtimes.csv')
-    with open(filename, 'w') as datafile:
-        writer = csv.writer(datafile, delimiter='&',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(header)
-      
-        for row in sorted_data:
-            if only_adapter_changed(row,last_row):
-                if row[4]=='fused':
-                    if row[5]=='FusedTimeStep' or row[5]=='ADERDGTimeStep': # comes before Prediction...
-                        result[9]  = row[10] # iterations
-                        timesteps = result[9]
-                        result[11] = str( float(row[11]) / float(timesteps) ) # normalised user time
-                    else:
-                        if row[5]=='Prediction':
-                            if single-core is False:
-                                iterations = float(row[10]) # one more iteration than reruns
-                                result[12] = iterations-1
-                                result[13] = str ( float(row[11]) / float(iterations) )
-                            
-                            timesteps = result[9]
-                            result[10] = str( float(result[11]) + float(result[12]) * float(result[13]) / float(timesteps) ) # normalised user time
-                if row[4]=='nonfused':
-                    if row[5]=='NeighbourDataMerging': # comes before Prediction,SolutionUpdate
-                        result[9]  = row[10] # iterations
-                    if row[5] in nonfusedAdapters:
-                        iterations = row[10]
-                        result[10] = str( float(result[10]) + float(row[11]) / float(iterations) ) # normalised user time
-            else:
-                writer.writerow(result)
-              
-                result = row[:5] + row[6:]
-                iterations = row[10]
-                result[9]  = '-1'
-                result[10] = '0 '  # normalised total runtime
-                result[11] = '0'   # normalised fused time step time
-                result.append('0') # predictor reruns
-                result.append('0.')# time per predictor rerun
-            
-            last_row = row
-        
-        # finalise (have to write a last time)
-        writer.writerow(result)
-    
-    datafile.close()
-
+    result = sorted(result, key=lambda x: (x[0],int(x[1]),x[2],x[3],x[4],int(x[5]),int(x[6]),int(x[7])))
+    writeTable(result,header,filename)
 ########################################################################
 # START OF THE PROGRAM
 ########################################################################
