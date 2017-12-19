615999ffb3dc9561746521f19497e58a62d60157
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2017-12-19 13:31:06 +0000

    Update the symlinks.

--- Benchmarks/coolmuc2/Elastic3D/create-symlinks.sh
+++ Benchmarks/coolmuc2/Elastic3D/create-symlinks.sh
@@ -7,3 +7,5 @@ ln -s ../../../ApplicationExamples/Elastic/Elastic3D/ElasticWaveEquation.h
 ln -s ../../../ApplicationExamples/Elastic/Elastic3D/topography.yaml
 
 ln -s ../../python/extractadaptertimes.py
+ln -s ../../python/extractunigridruntimes.py
+ln -s ../../python/plotsinglecoreruntimes.py
