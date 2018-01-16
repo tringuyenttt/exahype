8bc608a2f42942dab836e07ad75872e619185b03
Ben Hazelwood <benjamin.hazelwood@durham.ac.uk>
2018-01-16 12:13:41 +0000

    coolmuc3 CCZ4 multicore benchmarks

--- Benchmarks/coolmuc3_KNL/CCZ4/multicore/build-all-orders-no-output.sh
+++ Benchmarks/coolmuc3_KNL/CCZ4/multicore/build-all-orders-no-output.sh
@@ -5,16 +5,17 @@ spec=$directory/CCZ4-no-output.exahype
 
 cp $spec ${spec}_tmp
 
-for m in TBB None
+for m in OMP TBB NONE
 do
   make clean
   export SHAREDMEM=$m
+
   echo "SHAREDMEM=$SHAREDMEM"
   #read -p "press any key..."
 
-  for p in 3
+  for p in 3 6 9
   do 
-    rm *.o
+    rm *.o Fortran/*.o
     sed -i -r 's,order(\s+)const(\s+)=(\s+)([0-9]+),order\1const\2=\3'$p',' $spec
     cat $spec
     $directory/configure-no-output.sh
